affac4fd8b5c39341a50999251a21f8f0148139e
siddharthshah <shahsiddharth121@gmail.com>
2025-07-30 11:23:12 -0400

    Score preload and pose for 2nd spec (updated so it doesn't run into the spec on the ground)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/example/kotlin/PedroPath.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/example/kotlin/PedroPath.kt
@@ -25,25 +25,37 @@ class PedroPath: PedroOpMode(Claw, Slides, Wrist, Arm) {
     private val startPose = Pose(8.0, 64.0, Math.toRadians(180.0))
     //specimens deposit
     private val depositPose = Pose(39.0, 64.0, Math.toRadians(180.0))
-    //get ready to push the 1 spec
-    private val pushPose1= Pose(62.254, 17.9, Math.toRadians(180.0))
-    private val pushPose1control= Pose(34.01, 39.98, Math.toRadians(180.0))
+    //get ready to push the 1 spec p1
+    private val pushPose1= Pose(47.93, 40.17, Math.toRadians(180.0))
+    private val pushPose1control= Pose(27.25, 40.18, Math.toRadians(180.0))
+    //get ready to push the 2 spec p2
+    private val pushPose2= Pose(64.24, 28.44, Math.toRadians(180.0))
+    private val pushPose2control= Pose(70.61, 40.97, Math.toRadians(180.0))
+
+
 
 
     private lateinit var depositFirstSpec: PathChain
+    private lateinit var pushAndPick1: PathChain
     private lateinit var pushAndPick2: PathChain
+
 
 
     private fun buildPaths() {
         depositFirstSpec = follower.pathBuilder()
             .addPath(BezierLine(Point(startPose), Point(depositPose)))
-            .setLinearHeadingInterpolation(startPose.heading, depositPose.heading)
+            .setConstantHeadingInterpolation(startPose.heading)
             .build()
 
-        pushAndPick2 = follower.pathBuilder()
+        pushAndPick1 = follower.pathBuilder()
             .addPath(BezierCurve(Point(depositPose), Point(pushPose1control), Point(pushPose1)))
             .setLinearHeadingInterpolation(depositPose.heading, pushPose1.heading)
             .build()
+        pushAndPick2 = follower.pathBuilder()
+            .addPath(BezierCurve(Point(pushPose1), Point(pushPose2control), Point(pushPose2)))
+            .setConstantHeadingInterpolation(pushPose1.heading)
+            .build()
+
     }
 
 
